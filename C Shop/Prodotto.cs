dc9663583c811ca41b4a1341f02ccd6361bbc437
PC <PC@DESKTOP-LUEVN40>
2022-04-11 08:19:29 +0200

    Aggiungere i file di progetto.

---
+++ C Shop/Prodotto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Shop
+{
+    internal class Prodotto
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+     
+
+        public Prodotto(int codice)
+        {
+            this.codice = codice;
+        }
+
+        private Prodotto(int codice,string nome,string descrizione,double prezzo,double iva)
+        {
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+
+        }
+        internal class Prodotto1
+        {
+            private int codice;
+            private string nome;
+            private string descrizione;
+            private double prezzo;
+            private double iva;
+            private Prodotto1(int codice, string nome, string descrizione, double prezzo, double iva)
+        {
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+
+        }
+            //Metodo
+            public void CalcoloPrezzoBase(double prezzo)
+            {
+                double.Parse(Console.ReadLine());
+            }
+            //Metodo 
+            public void CalcoloPrezzoIva()
+            {
+ double   CalcoloPrezzoIva = prezzo * 0.22;
+                double.Parse(Console.ReadLine());
+            
+            }
+        public void Stampa()
+
+            {
+                Console.WriteLine(CalcoloPrezzoIva);
+
+            }
+        }
+    }
+}
---
+++ C Shop/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using C_Shop;
+
+Console.WriteLine("Inserisci il codice del prodotto: ");
+int codice = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci il nome del prodotto: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Inserisci la descrizione del prodotto: ");
+string descrizione = Console.ReadLine();
+Console.WriteLine("Inserisci il prezzo del prodotto: ");
+double prezzo = double.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci l'IVA del prodotto: ");
+double iva = double.Parse(Console.ReadLine());
+Prodotto prodotto1 = new Prodotto(codice);
+Prodotto prodotto2 = new Prodotto(codice);
+
+
+
+Console.WriteLine();
+Console.WriteLine("---Primo Prodotto---");
+prodotto1 = new Prodotto(codice);
+
+
+Console.WriteLine("---Secondo Prodotto---");
+prodotto2 = new Prodotto(codice);
+
+
+Random random = new Random();
+int numero = random.Next();
+Console.WriteLine(numero);
+
+
+
